84e0714d37d19510c61325f0b6c80f2475ce2111
swapnilnarad2000 <swapnilnarad2000@gmail.com>
2024-02-11 20:10:12 +0530

    Fixed CSS

--- src/info/Info.js
+++ src/info/Info.js
@@ -25,7 +25,7 @@ of what each of the values mean.
 export const info = {
     firstName: "Swapnil",
     lastName: "Narad",
-    initials: "Swapnil Narad", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "SN", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Software Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
